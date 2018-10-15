f1517c4d3f63dc230ed7fc42e6d4d87481b59387
JesseHouk900 <jessejameshouk@gmail.com>
2018-10-14 20:12:49 -0500

    still working on 11572, had solution, but took too long

--- UVa Judge Programs/UVa Judge Programs/11572.cpp
+++ UVa Judge Programs/UVa Judge Programs/11572.cpp
@@ -4,33 +4,43 @@
 using namespace std;
 
 int main() {
-    map<int, int> snowflakes;
-    int batches, flakes;
-    cin >> batches;
-    for (int b = 0; b < batches; b++) {
-        cin >> flakes;
-        for (int f = 0, flake; f < flakes; f++) {
-            cin >> flake;
-            map<int, int>::iterator sf;
-            for (auto it = sf = snowflakes.begin(); it != snowflakes.end(); ++it) {
-                if (it->second == flake) {
-                    sf = it;
-                }
-            }
-            cout << sf->first << "_" << sf->second << '\n';
-            if (sf == snowflakes.end()) {
-                snowflakes[f] = flake;
-            }
-            else {
-                snowflakes.erase(snowflakes.begin(), sf);
-                snowflakes[f] = flake;
-            }
-        }
-        for (auto it = snowflakes.begin(); it != snowflakes.end(); ++it) {
-            cout << it->first << " " << it->second << '\n';
-        }
-        cout << snowflakes.size() << '\n';
-        snowflakes.clear();
-    }
+	int batches, flakes;
+	map<int, int> snowflakes;
+	cin >> batches;
+	for (int b = 0, max; b < batches; b++) {
+		int sbegin, send = sbegin = 0;
+		cin >> flakes;
+		for (int f = max = 0, flake; f < flakes; f++) {
+			
+			cin >> flake;
+			map<int, int>::iterator sf;
+			int x = 0;
+			for (sf = snowflakes.begin(); sf != snowflakes.end() && sf->first < sbegin; ++x, ++sf) { continue; }
+			for (; sf != snowflakes.end() ; ++x, ++sf) { 
+				if (sf->second == flake) 
+					sbegin = x + 1; 
+			}
+			if (sf == snowflakes.end()) {
+				snowflakes[f] = flake;
+				send++;
+			}
+			else {
+				if (max < send - sbegin) {
+					max = send - sbegin;
+				}
+				//sbegin = sf->first;
+				if (sbegin >= send) {
+					send = sbegin + 1;
+				}
+				//snowflakes.erase(snowflakes.begin(), ++sf);
+				snowflakes[f] = flake;
+			}
+		}
+		if (max < send - sbegin) {
+			max = send - sbegin;
+		}
+		cout << max << '\n';
+		snowflakes.clear();
+	}
 
 }

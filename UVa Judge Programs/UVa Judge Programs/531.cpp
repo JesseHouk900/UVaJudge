764d56ad12f8d11c6cb59a1210f8e3feb9b5c4ba
JesseHouk900 <jessejameshouk@gmail.com>
2018-11-19 21:58:36 -0600

    not quite 531

--- UVa Judge Programs/UVa Judge Programs/531.cpp
+++ UVa Judge Programs/UVa Judge Programs/531.cpp
@@ -8,15 +8,17 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define p pair<int, string>
 
 using namespace std;
+
+p mymax(const p & p1, const p & p2);
 
 int main() {
 	string term;
 	bool isFirst = true;
 	vector<string> list1, list2 = list1 = vector<string>();
 	while (cin >> term) {
-		int i = 0;
 
 		if (term == "#") {
 			if (isFirst) {
@@ -25,28 +27,32 @@ int main() {
 			}
 			else {
 				// initialize a matrix to be pairs
-				vector<vector<pair<int, string> > > RSQMatrix = vector<vector<
-					pair<int, string> > >(list1.size(), vector<pair<int, string>
-					>(list2.size(), pair<int, string>(0, "")));
+				vector<vector<p > > RSQMatrix = vector<vector<
+					p > >(list1.size(), vector<p
+					>(list2.size(), p(0, "")));
 				
 				for (int r = 1; r < list1.size(); r++) {
+					// add brekpoint
 					for (int c = 1; c < list2.size(); c++) {
 						if (list1[r] == list2[c]) {
-							RSQMatrix[r][c] = pair<int, string>(RSQMatrix[r - 1]
-								[c - 1].first + 1, RSQMatrix[r - 1][c - 1].second
-								+ " " + list1[r]);
+							RSQMatrix[r][c] = p(RSQMatrix[r - 1]
+								[c - 1].first + 1, RSQMatrix[r - 1][c - 1].second);
+							if (RSQMatrix[r][c].first > 1) {
+								RSQMatrix[r][c].second += " ";
+							}
+							RSQMatrix[r][c].second += list1[r];
+
 						}
 						else {
-							RSQMatrix[r][c] = max(RSQMatrix[r][c - 1], RSQMatrix[r - 1][c]);
+							RSQMatrix[r][c] = mymax(RSQMatrix[r][c - 1], RSQMatrix[r - 1][c]);
 						}
 					}
 				}
-				pair<int, string> answer = RSQMatrix[list1.size() - 1][list2.size() - 1];
-				cout << answer.first << ' ' << answer.second << '\n';
+				p answer = RSQMatrix[list1.size() - 1][list2.size() - 1];
+				cout << answer.second << '\n';
 				list1 = list2 = vector<string>();
 				isFirst = true;
 			}
-			i = 0;
 		}
 		else {
 			if (isFirst) {
@@ -55,8 +61,18 @@ int main() {
 			else {
 				list2.push_back(term);
 			}
-			i++;
 		}
+
+	}
+}
+
+p mymax(const p & p1, const p & p2) {
+	int m = max(p1.first, p2.first);
+	if (m == p1.first) {
+		return p1;
+	}
+	else if (m == p2.first) {
+		return p2;
 
 	}
 }

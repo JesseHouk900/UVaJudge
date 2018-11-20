b6f2d59c7315395489471abd7ad82335dedffb95
JesseHouk900 <jessejameshouk@gmail.com>
2018-11-19 22:45:34 -0600

    531 attempt 3

--- UVa Judge Programs/UVa Judge Programs/531.cpp
+++ UVa Judge Programs/UVa Judge Programs/531.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-p mymax(const p & p1, const p & p2);
+p orderedMax(const p & p1, const p & p2);
 
 int main() {
 	string term;
@@ -28,27 +28,27 @@ int main() {
 			else {
 				// initialize a matrix to be pairs
 				vector<vector<p > > RSQMatrix = vector<vector<
-					p > >(list1.size(), vector<p
-					>(list2.size(), p(0, "")));
-				
-				for (int r = 1; r < list1.size(); r++) {
+					p > >(list2.size(), vector<p
+					>(list1.size(), p(0, "")));
+
+				for (int r = 1; r < list2.size(); r++) {
 					// add brekpoint
-					for (int c = 1; c < list2.size(); c++) {
-						if (list1[r] == list2[c]) {
+					for (int c = 1; c < list1.size(); c++) {
+						if (list2[r - 1] == list1[c - 1]) {
 							RSQMatrix[r][c] = p(RSQMatrix[r - 1]
 								[c - 1].first + 1, RSQMatrix[r - 1][c - 1].second);
 							if (RSQMatrix[r][c].first > 1) {
 								RSQMatrix[r][c].second += " ";
 							}
-							RSQMatrix[r][c].second += list1[r];
+							RSQMatrix[r][c].second += list2[r - 1];
 
 						}
 						else {
-							RSQMatrix[r][c] = mymax(RSQMatrix[r][c - 1], RSQMatrix[r - 1][c]);
+							RSQMatrix[r][c] = orderedMax(RSQMatrix[r][c - 1], RSQMatrix[r - 1][c]);
 						}
 					}
 				}
-				p answer = RSQMatrix[list1.size() - 1][list2.size() - 1];
+				p answer = RSQMatrix[list2.size() - 1][list1.size() - 1];
 				cout << answer.second << '\n';
 				list1 = list2 = vector<string>();
 				isFirst = true;
@@ -66,13 +66,42 @@ int main() {
 	}
 }
 
-p mymax(const p & p1, const p & p2) {
+p orderedMax(const p & p1, const p & p2) {
+	// get the max of the two integers of the pairs
 	int m = max(p1.first, p2.first);
-	if (m == p1.first) {
-		return p1;
-	}
-	else if (m == p2.first) {
+	// if the max euquals both values...
+	if (m == p1.first && m == p2.first) {
+		//
+		string s = min(p1.second, p2.second);
+		if (s == p1.second) {
+			return p1;
+		}
 		return p2;
 
 	}
+	else if (m == p1.first) {
+		return p1;
+	}
+	return p2;
 }
+	//p mymax(const p & p1, const p & p2) {
+	//	int m = max(p1.first, p2.first);
+	//	if (m == p1.first) {
+	//		if (m == p1.first) {
+	//			if (p1.second != "" && p2.second != "") {
+	//				string s = min(p1.second, p2.second);
+	//				if (s != p1.second) {
+	//					return p2;
+	//				}
+	//
+	//			}
+	//			else {
+	//				return 
+	//			}
+	//		return p1;
+	//		}
+	//	}
+	//	else if (m == p2.first) {
+	//		return p2;
+	//
+	//	}
